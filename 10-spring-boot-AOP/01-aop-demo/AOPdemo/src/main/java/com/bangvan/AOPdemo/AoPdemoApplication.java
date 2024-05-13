a4b7e2febee26fad131c15974ff41802667b1d6a
Bang Van <bangtranvan08@gmail.com>
2024-05-13 21:38:28 +0700

    update

--- 10-spring-boot-AOP/01-aop-demo/AOPdemo/src/main/java/com/bangvan/AOPdemo/AoPdemoApplication.java
+++ 10-spring-boot-AOP/01-aop-demo/AOPdemo/src/main/java/com/bangvan/AOPdemo/AoPdemoApplication.java
@@ -1,7 +1,10 @@
 package com.bangvan.AOPdemo;
 
+import com.bangvan.AOPdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AoPdemoApplication {
@@ -10,4 +13,18 @@ public class AoPdemoApplication {
 		SpringApplication.run(AoPdemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO accountDAO){
+		return  runner ->{
+			demoTheBeforeAdvice(accountDAO);
+		};
+	}
+
+
+	private void demoTheBeforeAdvice(AccountDAO accountDAO) {
+		accountDAO.addAccount();
+
+		System.out.println("\n let's call it again!");
+		accountDAO.addAccount();
+	}
 }
